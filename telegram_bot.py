9bd36fb29438c0ef82a358ca1b2757b3509b28ac
Lucas Vasques <lucasvc1996@gmail.com>
2024-06-03 16:26:18 -0300

    feat: Add explanation message to bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,17 +11,34 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, )
+    message = """
+        Olá, bem vindo ao corretor de rota Shopee!
+    
+Esse robô ainda está em fase de testes, peço paciência e qualquer problema peço que me procurem, meu nome é Lucas e estarei divulgando o robô no grupo.
+
+O intuito desse robô é melhorar a roterização de aplicativos como o Circuit, mas notem que podem ocorrer discrepancias caso o cliente tenha colocado o CEP errado.
+
+Para utilizar o robô é só compartilhar a planilha baixada do aplicatiov Driver Shopee. Depois abrir a planilha com o Circuit ou aplicativo de sua preferência, eu geralmente seleciono para aparecer as seguintes colunas: Qtd de Pacotes, Número dos pacotes e Complemento.
+
+Note que pode demorar 1 ou 2 minutos para processar a planilha.
+
+Digite /planilha caso ainda não sabia baixar a planilha do aplicativo Driver Shopee"""
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+async def planilha(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = """Para baixar a planilha é simples, abra o seu aplicativo.
+        1. Vá na aba de entregas pentendes, onde aparecem todos os pedidos a serem entregues.
+        2. Bem ao lado direito do botão de Mostrar no Mapa, tem o botão de baixar planilha.
+        3. Com a planilha baixada, é só compartilhar o arquivo nessa conversa pelo botão de clips de papel."""
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 async def doc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     new_file = await update.message.effective_attachment.get_file()
     raw_file_name = update.message.effective_attachment.file_name
-    print(raw_file_name)
     raw_file_path = 'data/raw/' + raw_file_name
-    print(raw_file_path)
     await new_file.download_to_drive(raw_file_path)
+
     processed_df = arrange_data.process_data(raw_file_path)
-    print('data/processed/' + raw_file_name + '_corrigido')
     processed_file_path = 'data/processed/corrigido_' + raw_file_name
     processed_df.to_excel(processed_file_path, index=False)
 
@@ -40,12 +57,15 @@ async def doc(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if not success:
         print("Failed to execute the command after 10 attempts.")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Não foi possível processar a planilha")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_API_KEY).build()
 
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler('planilha', planilha))
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT, start))
+
 
     application.add_handler(MessageHandler(filters.ATTACHMENT, doc))
 
